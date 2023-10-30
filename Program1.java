aff052e3cb87cd2210f893afae3c782d41ae5a11
मयंक <126972040+MayankDevil@users.noreply.github.com>
2023-10-30 15:00:52 +0000

    Create Program1.java

---
+++ Program1.java
@@ -0,0 +1,20 @@
+class Program1
+{
+  public static void main(String args[])
+  {
+    short N = new java.util.Scanner(System.in).nextShort();
+
+    if (N%2 != 0)
+    {
+      System.out.println("Weird");
+    }
+    else
+    {
+      if ()
+      {
+        System.out.println();
+      }
+    }
+
+  }
+}
